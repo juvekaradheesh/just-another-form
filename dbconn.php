c01f46d88167ef514146c16fb3e188380d27fff0
juvekaradheesh <juvekaradheesh@gmail.com>
2017-05-06 11:09:09 +0530

    basic files added

---
+++ dbconn.php
@@ -0,0 +1,7 @@
+<?php
+    define('SERVERNAME',"localhost");
+    define('USERNAME',"root");
+    define('PASSWORD',"");
+    define('DBNAME',"wordpress");
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+?>
